515b164c0f3e27e129dda67b93ca3416eff0da64
David Conner <dconner.pro@gmail.com>
2016-01-31 14:31:35 -0500

    UserCreateService: add profile_photo_url to user model

--- app/services/create_user_service.rb
+++ app/services/create_user_service.rb
@@ -9,7 +9,10 @@ class CreateUserService
     tweet_user = tweet['user']
     twitter_name = tweet_user['name']
     twitter_user_id = tweet_user['screen_name']
-    User.create!(username: twitter_name, twitter_user_id: twitter_user_id)
+
+    User.create!(username: twitter_name,
+                 twitter_user_id: twitter_user_id,
+                 profile_photo_url: tweet_user['profile_image_url'])
   end
 
 end
